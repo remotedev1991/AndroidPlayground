e41238f2315c31ca20776d5c49a78cf4a28a0bd5
mansal-97 <lakshmimanasag616@gmail.com>
2026-03-12 16:27:16 -0500

    Spinner Example

---
+++ app/src/main/java/com/nak/androidplayground/SpinnerActivity.kt
@@ -0,0 +1,29 @@
+package com.nak.androidplayground
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SpinnerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_spinner)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val spinner=findViewById<Spinner>(R.id.spinner)
+        val countries = listOf<String>( "India","USA","South Korea","Bangladesh","New ZeaLand")
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item,
+            countries)
+
+        spinner.adapter = adapter
+    }
+}
